b88bbcdbb18280d22d43d3250e60b30fbc09c55f
Vais Salikhov <vsalikhov@gmail.com>
2023-04-17 22:27:57 -0400

    Make a dynamic form

--- lib/live_view_studio_web/live/sandbox_live.ex
+++ lib/live_view_studio_web/live/sandbox_live.ex
@@ -1,7 +1,22 @@
 defmodule LiveViewStudioWeb.SandboxLive do
   use LiveViewStudioWeb, :live_view
 
+  import Number.Currency, only: [number_to_currency: 1]
+  import Number.Delimit, only: [number_to_delimited: 1]
+
+  alias LiveViewStudio.Sandbox
+
   def mount(_params, _session, socket) do
+    socket =
+      assign(
+        socket,
+        length: 0,
+        width: 0,
+        depth: 0,
+        weight: 0,
+        price: nil
+      )
+
     {:ok, socket}
   end
 
@@ -9,41 +24,68 @@ defmodule LiveViewStudioWeb.SandboxLive do
     ~H"""
     <h1>Build A Sandbox</h1>
     <div id="sandbox">
-      <form>
+      <form phx-change="calculate" phx-submit="get-quote">
         <div class="fields">
           <div>
             <label for="length">Length</label>
             <div class="input">
-              <input type="number" name="length" value="1" />
+              <input type="number" name="length" value={@length} />
               <span class="unit">feet</span>
             </div>
           </div>
           <div>
             <label for="width">Width</label>
             <div class="input">
-              <input type="number" name="width" value="2" />
+              <input type="number" name="width" value={@width} />
               <span class="unit">feet</span>
             </div>
           </div>
           <div>
             <label for="depth">Depth</label>
             <div class="input">
-              <input type="number" name="depth" value="3" />
+              <input type="number" name="depth" value={@depth} />
               <span class="unit">inches</span>
             </div>
           </div>
         </div>
         <div class="weight">
-          You need 43 pounds of sand 🏝
+          You need <%= number_to_delimited(@weight) %> pounds of sand 🏝
         </div>
         <button type="submit">
           Get A Quote
         </button>
       </form>
-      <div class="quote">
-        Get your personal beach today for only $65
+      <div :if={@price} class="quote">
+        Get your personal beach today for only <%= number_to_currency(
+          @price
+        ) %>
       </div>
     </div>
     """
+  end
+
+  def handle_event("calculate", params, socket) do
+    %{"length" => l, "width" => w, "depth" => d} = params
+
+    weight = Sandbox.calculate_weight(l, w, d)
+
+    socket =
+      assign(
+        socket,
+        length: l,
+        width: w,
+        depth: d,
+        weight: weight,
+        price: nil
+      )
+
+    {:noreply, socket}
+  end
+
+  def handle_event("get-quote", _params, socket) do
+    weight = socket.assigns.weight
+    price = Sandbox.calculate_price(weight)
+    socket = assign(socket, price: price)
+    {:noreply, socket}
   end
 end
