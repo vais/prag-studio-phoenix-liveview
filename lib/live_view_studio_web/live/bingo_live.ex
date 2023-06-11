8a192c00406571a5742fb00da15659185038e966
Vais Salikhov <vsalikhov@gmail.com>
2023-06-11 14:48:10 -0400

    Do the Track Presence for Bingo Players exercise

--- lib/live_view_studio_web/live/bingo_live.ex
+++ lib/live_view_studio_web/live/bingo_live.ex
@@ -1,11 +1,23 @@
 defmodule LiveViewStudioWeb.BingoLive do
   use LiveViewStudioWeb, :live_view
+  alias LiveViewStudioWeb.Presence
+
+  @topic "users:bingo"
 
   def mount(_params, _session, socket) do
     socket = assign(socket, number: nil, numbers: all_numbers())
+    socket = assign(socket, :presences, Presence.list_users(@topic))
 
     if connected?(socket) do
+      :ok = Presence.subscribe(@topic)
+
+      {:ok, _ref} =
+        Presence.track_user(socket.assigns.current_user, @topic, %{
+          timestamp: Timex.now("America/New_York") |> Timex.format!("%H:%M", :strftime)
+        })
+
       :timer.send_interval(1000, self(), :pick)
+
       {:ok, pick(socket)}
     else
       {:ok, socket}
@@ -17,6 +29,14 @@ defmodule LiveViewStudioWeb.BingoLive do
     <h1>Bingo Boss 📢</h1>
     <h2><%= @current_user.email %></h2>
     <div id="bingo">
+      <div class="users">
+        <ul>
+          <li :for={{_user_id, meta} <- @presences}>
+            <span class="username"><%= meta.username %></span>
+            <span class="timestamp"><%= meta.timestamp %></span>
+          </li>
+        </ul>
+      </div>
       <div class="number">
         <%= @number %>
       </div>
@@ -51,5 +71,9 @@ defmodule LiveViewStudioWeb.BingoLive do
 
   def handle_info(:pick, socket) do
     {:noreply, pick(socket)}
+  end
+
+  def handle_info(%{event: "presence_diff", payload: diff}, socket) do
+    {:noreply, Presence.handle_diff(socket, diff)}
   end
 end
